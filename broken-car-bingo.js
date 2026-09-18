77f64cda2c263907edffb4e52f2ee863d0665f8c
rsnwms2tpm-lang <rsnwms2tpm@privaterelay.appleid.com>
2026-09-18 22:08:52 +0100

    Expose current secure Bingo state to full-screen view

--- broken-car-bingo.js
+++ broken-car-bingo.js
@@ -125,6 +125,7 @@
     if(key!==eventKey){eventKey=key;refresh(true);return}
     refresh(false);
   }
+  window.__tdhGetBingoState=()=>bingo;
   window.__tdhRefreshBingo=()=>{lastRenderSig='';if(bingo){render(true);return Promise.resolve()}return refresh(true)};
   setInterval(tick,4000);
   window.addEventListener('focus',()=>refresh(true));
